eca8266b2e16031a2f7d7c7245f90662f32b0f6c
shihyihuang <shua0107@student.monash.edu>
2023-12-17 17:22:40 +1100

    improved grade

--- academe/src/Pages/grade.js
+++ academe/src/Pages/grade.js
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 // localStorage.removeItem("unitAvg");
 const Grade = () => {
   const navigate = useNavigate();
-  const header = ["semester", "unit", "average"];
+  const header = ["semester", "unit", "grade"];
 
   const handleClick = (event) => {
     const id = event.target.id;
@@ -18,62 +18,27 @@ const Grade = () => {
     JSON.parse(localStorage.getItem("unitAvg")) || []
   );
 
-  const data = [];
-
   const retrieveOverallData = () => {
-    const sem1Unit = [];
-    const sem2Unit = [];
-    const sem3Unit = [];
-    const sem4Unit = [];
-    var sem1GradeSum = 0;
-    var sem2GradeSum = 0;
-    var sem3GradeSum = 0;
-    var sem4GradeSum = 0;
-    items.map((item) => {
+    const temp = {};
+    items.forEach((item) => {
       const semester = item.id.split("-")[0];
       const unit = item.id.split("-")[1];
-      switch (semester) {
-        case "sem1":
-          sem1Unit.push(unit);
-          sem1GradeSum += item.average;
-          break;
-        case "sem2":
-          sem2Unit.push(unit);
-          sem2GradeSum += item.average;
-          break;
-        case "sem3":
-          sem3Unit.push(unit);
-          sem3GradeSum += item.average;
-          break;
-        case "sem4":
-          sem4Unit.push(unit);
-          sem4GradeSum += item.average;
-          break;
-        default:
-          return [];
+
+      if (!temp[semester]) {
+        temp[semester] = { units: [], gradeSum: 0 };
       }
+      temp[semester].units.push(unit);
+      temp[semester].gradeSum += item.average;
     });
-    data.push({
-      semester: "1",
-      unit: sem1Unit.toString(),
-      average: Math.round((sem1GradeSum / sem1Unit.length) * 100) / 100,
-    });
-    data.push({
-      semester: "2",
-      unit: sem2Unit.toString(),
-      average: Math.round((sem2GradeSum / sem2Unit.length) * 100) / 100,
-    });
-    data.push({
-      semester: "3",
-      unit: sem3Unit.toString(),
-      average: Math.round((sem3GradeSum / sem3Unit.length) * 100) / 100,
-    });
-    data.push({
-      semester: "4",
-      unit: sem4Unit.toString(),
-      average: Math.round((sem4GradeSum / sem4Unit.length) * 100) / 100,
-    });
-    return data;
+
+    return Object.keys(temp).map((semester) => ({
+      semester: semester,
+      unit: temp[semester].units.join(", "),
+      grade:
+        Math.round(
+          (temp[semester].gradeSum / temp[semester].units.length) * 100
+        ) / 100,
+    }));
   };
 
   console.log("data: ", JSON.stringify(retrieveOverallData()));
@@ -83,6 +48,7 @@ const Grade = () => {
   };
 
   const renderRows = () => {
+    const data = retrieveOverallData();
     return data.map((item, dataIndex) => (
       <tr key={`row-${dataIndex}`}>
         {header.map((header, headerIndex) => (
@@ -100,7 +66,16 @@ const Grade = () => {
   return (
     <div className="container">
       <ButtonGroup aria-label="Basic example" style={{ margin: "50px" }}>
-        <Button variant="secondary" id="sem1" onClick={handleClick}>
+        {[1, 2, 3, 4].map((semester) => (
+          <Button
+            key={semester}
+            variant="secondary"
+            id={`sem${semester}`}
+            onClick={handleClick}>
+            Semester {semester}
+          </Button>
+        ))}
+        {/* <Button variant="secondary" id="sem1" onClick={handleClick}>
           Semester1
         </Button>
         <Button variant="secondary" id="sem2" onClick={handleClick}>
@@ -111,7 +86,7 @@ const Grade = () => {
         </Button>
         <Button variant="secondary" id="sem4" onClick={handleClick}>
           Semester4
-        </Button>
+        </Button> */}
       </ButtonGroup>
       <table className="table table-striped table-hover">
         <thead>
@@ -123,7 +98,62 @@ const Grade = () => {
   );
 };
 
-console.log("unitAvg: ", localStorage.getItem("unitAvg"));
-console.log("sem1: ", localStorage.getItem("sem1"));
-
 export default Grade;
+
+// const data = [];
+
+// const retrieveOverallData = () => {
+//   const sem1Unit = [];
+//   const sem2Unit = [];
+//   const sem3Unit = [];
+//   const sem4Unit = [];
+//   var sem1GradeSum = 0;
+//   var sem2GradeSum = 0;
+//   var sem3GradeSum = 0;
+//   var sem4GradeSum = 0;
+//   items.map((item) => {
+//     const semester = item.id.split("-")[0];
+//     const unit = item.id.split("-")[1];
+//     switch (semester) {
+//       case "sem1":
+//         sem1Unit.push(unit);
+//         sem1GradeSum += item.average;
+//         break;
+//       case "sem2":
+//         sem2Unit.push(unit);
+//         sem2GradeSum += item.average;
+//         break;
+//       case "sem3":
+//         sem3Unit.push(unit);
+//         sem3GradeSum += item.average;
+//         break;
+//       case "sem4":
+//         sem4Unit.push(unit);
+//         sem4GradeSum += item.average;
+//         break;
+//       default:
+//         return [];
+//     }
+//   });
+//   data.push({
+//     semester: "1",
+//     unit: sem1Unit.toString(),
+//     average: Math.round((sem1GradeSum / sem1Unit.length) * 100) / 100,
+//   });
+//   data.push({
+//     semester: "2",
+//     unit: sem2Unit.toString(),
+//     average: Math.round((sem2GradeSum / sem2Unit.length) * 100) / 100,
+//   });
+//   data.push({
+//     semester: "3",
+//     unit: sem3Unit.toString(),
+//     average: Math.round((sem3GradeSum / sem3Unit.length) * 100) / 100,
+//   });
+//   data.push({
+//     semester: "4",
+//     unit: sem4Unit.toString(),
+//     average: Math.round((sem4GradeSum / sem4Unit.length) * 100) / 100,
+//   });
+//   return data;
+// };
