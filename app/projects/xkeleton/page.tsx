906d722a149e79aae9a0e0d301978f965429c162
Claire Huang <100766288+shihyihuang@users.noreply.github.com>
2024-08-18 17:37:12 +1000

    update xkelenton content

--- app/projects/xkeleton/page.tsx
+++ app/projects/xkeleton/page.tsx
@@ -10,46 +10,65 @@ const page = () => {
             src="/projects/xkeleton/xkeleton.png"
             className="rounded-lg" />
           <div className="absolute left-4 right-4 md:left-6 md:right-6  lg:left-10 lg:right-10  top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide4" className="btn-circle bg-neutral text-primary rounded-full p-3 text-xs md:text-lg lg:text-xl flex items-center justify-center
-              w-btn-sm h-btn-sm md:w-btn-md md:h-btn-md lg:w-btn-lg lg:h-btn-lg">
-          ❮
-          </a>
-          <a href="#slide2" className="btn-circle bg-neutral text-primary rounded-full p-3 text-xs md:text-lg lg:text-xl flex items-center justify-center
-                       w-btn-sm h-btn-sm md:w-btn-md md:h-btn-md lg:w-btn-lg lg:h-btn-lg">
-            ❯
-          </a>
+            <a href="#slide4" className="btn-circle bg-neutral text-primary rounded-full p-3 text-xs md:text-lg lg:text-xl flex items-center justify-center
+                w-btn-sm h-btn-sm md:w-btn-md md:h-btn-md lg:w-btn-lg lg:h-btn-lg">
+            ❮
+            </a>
+            <a href="#slide2" className="btn-circle bg-neutral text-primary rounded-full p-3 text-xs md:text-lg lg:text-xl flex items-center justify-center
+                        w-btn-sm h-btn-sm md:w-btn-md md:h-btn-md lg:w-btn-lg lg:h-btn-lg">
+              ❯
+            </a>
           </div>
         </div>
+
         <div id="slide2" className="carousel-item relative w-full flex justify-center">
           <img
             src="/projects/xkeleton/xkeleton2.png"
             className="rounded-lg" />
           <div className="absolute left-4 right-4 md:left-6 md:right-6 lg:left-10 lg:right-10 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide1" className="btn-circle bg-neutral text-primary rounded-full p-3 text-xs md:text-lg lg:text-xl flex items-center justify-center
-              w-btn-sm h-btn-sm md:w-btn-md md:h-btn-md lg:w-btn-lg lg:h-btn-lg">
-          ❮
-          </a>
-          <a href="#slide3" className="btn-circle bg-neutral text-primary rounded-full p-2 text-xs md:text-lg lg:text-xl  flex items-center justify-center
-                        w-btn-sm h-btn-sm md:w-btn-md md:h-btn-md lg:w-btn-lg lg:h-btn-lg">
-            ❯
-          </a>
+            <a href="#slide1" className="btn-circle bg-neutral text-primary rounded-full p-3 text-xs md:text-lg lg:text-xl flex items-center justify-center
+                w-btn-sm h-btn-sm md:w-btn-md md:h-btn-md lg:w-btn-lg lg:h-btn-lg">
+            ❮
+            </a>
+            <a href="#slide3" className="btn-circle bg-neutral text-primary rounded-full p-2 text-xs md:text-lg lg:text-xl  flex items-center justify-center
+                          w-btn-sm h-btn-sm md:w-btn-md md:h-btn-md lg:w-btn-lg lg:h-btn-lg">
+              ❯
+            </a>
           </div>
         </div>
+
         <div id="slide3" className="carousel-item relative w-full flex justify-center">
           <img
             src="/projects/xkeleton/xkeleton3.png"
             className="rounded-lg" />
           <div className="absolute left-4 right-4 md:left-6 md:right-6 lg:left-10 lg:right-10 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide2" className="btn-circle bg-neutral text-primary rounded-full p-2 text-xs md:text-lg lg:text-xl flex items-center justify-center
-              w-btn-sm h-btn-sm md:w-btn-md md:h-btn-md lg:w-btn-lg lg:h-btn-lg">
-          ❮
-          </a>
-          <a href="#slide1" className="btn-circle bg-neutral text-primary rounded-full p-2 text-xs md:text-lg lg:text-xl flex items-center justify-center
-                        w-btn-sm h-btn-sm md:w-btn-md md:h-btn-md lg:w-btn-lg lg:h-btn-lg">
-            ❯
-          </a>
+            <a href="#slide2" className="btn-circle bg-neutral text-primary rounded-full p-2 text-xs md:text-lg lg:text-xl flex items-center justify-center
+                w-btn-sm h-btn-sm md:w-btn-md md:h-btn-md lg:w-btn-lg lg:h-btn-lg">
+            ❮
+            </a>
+            <a href="#slide4" className="btn-circle bg-neutral text-primary rounded-full p-2 text-xs md:text-lg lg:text-xl flex items-center justify-center
+                          w-btn-sm h-btn-sm md:w-btn-md md:h-btn-md lg:w-btn-lg lg:h-btn-lg">
+              ❯
+            </a>
           </div>
         </div>
+
+        <div id="slide4" className="carousel-item relative w-full flex justify-center">
+          <img
+            src="/projects/xkeleton/xkeleton4.png"
+            className="rounded-lg" />
+          <div className="absolute left-4 right-4 md:left-6 md:right-6  lg:left-10 lg:right-10  top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a href="#slide3" className="btn-circle bg-neutral text-primary rounded-full p-3 text-xs md:text-lg lg:text-xl flex items-center justify-center
+                w-btn-sm h-btn-sm md:w-btn-md md:h-btn-md lg:w-btn-lg lg:h-btn-lg">
+            ❮
+            </a>
+            <a href="#slide1" className="btn-circle bg-neutral text-primary rounded-full p-3 text-xs md:text-lg lg:text-xl flex items-center justify-center
+                        w-btn-sm h-btn-sm md:w-btn-md md:h-btn-md lg:w-btn-lg lg:h-btn-lg">
+              ❯
+            </a>
+          </div>
+        </div>
+
       </div>
 
       <div className="flex justify-center mb-10 md:mb-20 mt-5 lg:mt-20 mx-4">
@@ -61,7 +80,7 @@ const page = () => {
                         title="Role-based authentication"
                         content={
                             <>
-                              role-based authentication mechanism with two roles (practitioner & patient) to facilitate CRUD
+                              Role-based access control tailors user permissions according to specific roles, ensuring that each user, from practitioners to patients, has the appropriate access and functionalities necessary to efficiently manage CRUD operations.
                             </>
                         }
                     />
@@ -69,23 +88,21 @@ const page = () => {
                         bgColor="bg-neutral"
                         titleColor="text-warning"
                         textColor="text-info"
-                        title="Aggregated rating score"
+                        title="Interactive Table Data"
                         content={
                             <>
-                              aggregated rating score
+                              Interactive tables allow practitioners to easily manipulate and visualize data directly from the database, enhancing their ability to understand and work with patient information.
                             </>
                         }
                     />
-                    {/* <ProjectAccordion
+                    <ProjectAccordion
                         bgColor="bg-warning"
                         titleColor="text-neutral"
                         textColor="text-info"
-                        title="Intake Calculator"
+                        title="Bulk Email"
                         content={
                             <>
-                                Users can enter their intended grocery items, and the system will display the nutrition information, 
-                                including a healthy rating and allergen content, if included. 
-                                Users can also swipe to view detailed macronutrient information.
+                              Practitioners can send emails directly from the application, including attachments, which is essential for effective communication. Additionally, the application supports sending bulk emails to selected users, facilitating large-scale communication efforts.
                             </>
                         }
                     />
@@ -93,12 +110,10 @@ const page = () => {
                         bgColor="bg-neutral"
                         titleColor="text-warning"
                         textColor="text-info"
-                        title="Meal Planner"
+                        title="Geo-location"
                         content={
                             <>
-                                Explore a wide range of personalized meal options by searching for recipes based on ingredients, cuisine, 
-                                or dietary preferences such as vegan or gluten-free. Additionally, you can enter your recommended calorie intake 
-                                and allergies to receive balanced meal plans for breakfast, lunch, and dinner, tailored to your specific needs.
+                              The inclusion of interactive maps enhances the user experience by providing spatial context and functionality, including place searches and detailed navigation.
                             </>
                         }
                     />
@@ -106,14 +121,14 @@ const page = () => {
                         bgColor="bg-warning"
                         titleColor="text-neutral"
                         textColor="text-info"
-                        title="Healthy Diet Visualizer"
+                        title="Security and Validation"
                         content={
                             <>
-                                Select a food to discover which part of your body it benefits and learn the effects of deficiencies. 
-                                This feature helps foster a positive relationship with food.
+                              Implementing rigorous input validation and sanitization across both the frontend and backend protects against SQL injections, XSS (Cross-Site Scripting), and CSRF (Cross-Site Request Forgery). <br/>
+                              The integration of the Ganss.Xss library sanitizes HTML content, while the use of ASP.NET MVC’s ValidateAntiForgeryToken attribute shields interactions from common web threats. These security protocols, meticulously applied to all user inputs including email communications, ensure that data processed through the application is consistently secure and reliable.
                             </>
                         }
-                    /> */}
+                    />
                 </div>
             </div>
     </div>
